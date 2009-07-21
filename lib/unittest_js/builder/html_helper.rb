071c89414e37e1299699a5c167124e544c9bed54
Tobie Langel <tobie.langel@gmail.com>
2009-07-21 17:01:17 +0200

    Adding default cache busting timestamps to assets. Should fix weird caching issues in Safari 4.

--- lib/unittest_js/builder/html_helper.rb
+++ lib/unittest_js/builder/html_helper.rb
@@ -2,11 +2,15 @@ module UnittestJS
   module Builder
     module HtmlHelper
       def to_script_tag(filename)
-        "<script src=\"#{filename}\" type=\"text/javascript\" charset=\"utf-8\"></script>"
+        "<script src=\"#{filename}?#{cache_buster}\" type=\"text/javascript\" charset=\"utf-8\"></script>"
       end
 
       def to_link_tag(filename)
-        "<link rel=\"stylesheet\" href=\"#{filename}\" type=\"text/css\" />"
+        "<link rel=\"stylesheet\" href=\"#{filename}?#{cache_buster}\" type=\"text/css\" />"
+      end
+      
+      def cache_buster
+        @cache_buster ||= Time.now.to_i.to_s
       end
     end
   end
